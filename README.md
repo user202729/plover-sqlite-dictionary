768fe8df2df57fc2f0e6ab4ab66837b09cef8436
user202729 <25191436+user202729@users.noreply.github.com>
2021-07-15 13:57:52 +0700

    Implement SQL dictionary format

--- README.md
+++ README.md
@@ -7,14 +7,21 @@ SQLite-based dictionary format for Plover. Consumes less memory for large dictio
   suffix rules and Qt modules still take a considerable amount of RAM)
 * Faster loading/startup speed.
 
+### Note
+
+The SQL dictionary format is not completely compatible with Plover's API specification,
+the plugin may fail to work properly in future Plover versions.
+
 ### Usage
 
 There are two possible file formats:
 
-* SQLite-based: `.sql` extension. (*not implemented*)
+* SQLite-based: `.sql` extension.
 
    Use the "New dictionary" feature in Plover to create one, or "Save dictionary as..." feature
    to convert from other dictionary formats.
+
+   Note: the file size is larger than the size of a JSON dictionary.
 
 * JSON: stored as plain JSON file on disk. `.jssql` extension.
 
--- plover_sqlite_dictionary/__init__.py
+++ plover_sqlite_dictionary/__init__.py
@@ -1,9 +1,11 @@
 # vim: set fileencoding=utf-8 :
 
 import typing
-from typing import List, Tuple, Iterable, Set, Optional, Dict
+from typing import List, Tuple, Iterable, Set, Optional, Dict, TypeVar, Callable, Any
 import sqlite3
 import json
+import functools
+import threading
 
 from plover.steno_dictionary import StenoDictionary  # type: ignore
 
@@ -15,54 +17,90 @@ def outline_to_str(outline: Outline)->str:
 def str_to_outline(outline_str: str)->Outline:
 	return tuple(outline_str.split("/"))
 
-class SQLiteDictionary(StenoDictionary):
+F=TypeVar("F", bound=Callable)
+def with_lock(f: F)->F:
+	@functools.wraps(f)
+	def wrap(self, *args, **kwargs)->Any:
+		result=self._lock.acquire(timeout=1)
+		assert result
+		try:
+			return f(self, *args, **kwargs)
+		finally:
+			self._lock.release()
+	return typing.cast(F, wrap)
 
+
+class SQLiteDictionaryBase(StenoDictionary):
 	def __init__(self)->None:
 		super().__init__()
+		self._lock=threading.Lock()  # use this whenever connection is accessed.
 		self._connection: Optional[sqlite3.Connection]=None
-		self._cursor: Optional[sqlite3.Cursor]=None
+		self._connection_path: str=""
+		self._cursor_value: Optional[sqlite3.Cursor]=None
 
-	def _get_cursor(self)->sqlite3.Cursor:
-		if self._cursor is None:
-			assert self._connection is None
-			self._connection=sqlite3.connect(":memory:",
-					check_same_thread=False,
-					)
-			self._cursor=self._connection.cursor()
-			self._cursor.execute("""
-				create table dict(
-					outline text primary key not null,
-					translation text not null,
-					length int not null
-				);
-				""")
-		return self._cursor
+	def _connect_unlocked(self, filename: str=":memory:")->None:
+		if self._connection is not None and self._connection_path==filename:
+			return
+		self._connection=sqlite3.connect(filename, check_same_thread=False)
+		self._connection_path=filename
+		self._cursor_value=self._connection.cursor()
+		self._cursor_value.execute("""
+			create table if not exists dict(
+				outline text primary key not null,
+				translation text not null,
+				length int not null
+			);
+			""")
 
-	def _compute_longest_key(self)->int:
-		cursor=self._get_cursor()
-		cursor.execute("create index if not exists dict_length on dict (length)")
-		return cursor.execute("select max(length) from dict").fetchone()[0] or 0
+	@with_lock
+	def _connect(self, filename: str=":memory:")->None:
+		self._connect_unlocked(filename)
 
+	def _commit_unlocked(self)->None:
+		assert self._connection
+		self._connection.commit()
+
+	@with_lock
+	def _commit(self)->None:
+		self._commit_unlocked()
+
+	@property
+	def _cursor(self)->sqlite3.Cursor:
+		assert self._cursor_value is not None, (self.path, self._connection_path)
+		assert self._lock.locked()
+		return self._cursor_value
+
+	def _compute_longest_key_unlocked(self)->int:
+		self._cursor.execute("create index if not exists dict_length on dict (length)")
+		return self._cursor.execute("select max(length) from dict").fetchone()[0] or 0
+
+	@with_lock
+	def compute_longest_key(self)->int:
+		return self._compute_longest_key_unlocked()
+
+	@with_lock
 	def items(self)->Iterable[Tuple[Outline, str]]:
-		cursor=self._get_cursor()
 		return (
 				(str_to_outline(outline), translation)
 				for outline, translation in
-				cursor.execute("select outline, translation from dict")
+				self._cursor.execute("select outline, translation from dict")
 				)
 
-	def clear(self)->None:
-		cursor=self._get_cursor()
-		cursor.execute("delete from dict")
+	def _clear_unlocked(self)->None:
+		self._cursor.execute("delete from dict")
 		self._longest_key=0
 
+	@with_lock
+	def clear(self)->None:
+		self._clear_unlocked()
+
+	@with_lock
 	def update_str(self, data: Iterable[Tuple[str, str]])->None:
-		cursor=self._get_cursor()
-		cursor.executemany("replace into dict values (?, ?, ?)", (
+		self._cursor.executemany("replace into dict values (?, ?, ?)", (
 			(outline, translation, len(outline))
 			for outline, translation in data
 			))
-		self._longest_key=self._compute_longest_key()
+		self._longest_key=self._compute_longest_key_unlocked()
 
 	def update(self, *args)->None:
 		assert not self.readonly
@@ -77,35 +115,20 @@ class SQLiteDictionary(StenoDictionary):
 			for outline, translation in iterable
 			)
 
+	@with_lock
 	def __setitem__(self, outline: Outline, translation: str)->None:
-		cursor=self._get_cursor()
-		cursor.execute("replace into dict values (?, ?, ?)",
+		self._cursor.execute("replace into dict values (?, ?, ?)",
 				(outline_to_str(outline), translation, len(outline)))
-		self._longest_key=self._compute_longest_key()
+		self._longest_key=self._compute_longest_key_unlocked()
 
+	@with_lock
 	def __delitem__(self, outline: Outline)->None:
-		cursor=self._get_cursor()
-		cursor.execute("delete from dict where outline=?", (outline_to_str(outline),))
-		num_changes: int=cursor.execute("select changes()").fetchone()[0]
+		self._cursor.execute("delete from dict where outline=?", (outline_to_str(outline),))
+		num_changes: int=self._cursor.execute("select changes()").fetchone()[0]
 		assert 0<=num_changes<=1
 		if num_changes==0:
 			raise KeyError
-		self._longest_key=self._compute_longest_key()
-
-	def _load(self, filename: str)->None:
-		self.clear()
-		with open(filename, "r") as f:
-			data: Dict[str, str]=json.load(f)
-			self.update_str(data.items())
-
-	def _save(self, filename: str)->None:
-		cursor=self._get_cursor()
-		json.dump(
-				dict(cursor.execute("select outline, translation from dict")),
-				open(filename, "w"),
-				indent=0,
-				ensure_ascii=False,
-				)
+		self._longest_key=self._compute_longest_key_unlocked()
 
 	def __contains__(self, outline)->bool:
 		try:
@@ -115,10 +138,10 @@ class SQLiteDictionary(StenoDictionary):
 			pass
 		return False
 
+	@with_lock
 	def __getitem__(self, outline: Outline)->str:
-		cursor=self._get_cursor()
 		result: List[Tuple[str]]=[*
-				cursor.execute("select translation from dict where outline=?",
+				self._cursor.execute("select translation from dict where outline=?",
 					(outline_to_str(outline),))
 				]
 		assert len(result)<=1
@@ -131,9 +154,63 @@ class SQLiteDictionary(StenoDictionary):
 		except KeyError:
 			return fallback
 
+	@with_lock
 	def reverse_lookup(self, translation: str)->Set[Outline]:
-		cursor=self._get_cursor()
-		cursor.execute("create index if not exists dict_translation on dict (translation)")
+		self._cursor.execute("create index if not exists dict_translation on dict (translation)")
 		return {str_to_outline(outline_str) for [outline_str] in
-				cursor.execute("select outline from dict where translation=?", (translation,))
+				self._cursor.execute("select outline from dict where translation=?", (translation,))
 				}
+
+
+class SQLiteDictionary(SQLiteDictionaryBase):
+	def _load(self, filename: str)->None:
+		path_was_none=False
+		if self.path is None:  # type: ignore
+			path_was_none=True
+			self.path=filename
+		self._connect(filename)
+		self._longest_key=self.compute_longest_key()
+		if path_was_none:
+			self.path=None  # type: ignore
+
+	@property
+	def _cursor(self)->sqlite3.Cursor:
+		assert self._lock.locked()
+		if not self._connection_path:
+			self._connect_unlocked(self.path)
+			# the imaginary dict in RAM is empty
+			self._clear_unlocked()
+		else:
+			assert self._connection_path==self.path, (self._connection_path, self.path)
+		assert self._cursor_value is not None
+		return self._cursor_value
+
+	@with_lock
+	def save(self)->None:
+		self._cursor
+		assert self._connection is not None
+		self._commit_unlocked()
+
+	def _save(self)->None:
+		raise NotImplementedError
+
+
+class SQLiteJSONDictionary(SQLiteDictionaryBase):
+	def __init__(self)->None:
+		super().__init__()
+		self._connect()
+
+	def _load(self, filename: str)->None:
+		self.clear()
+		with open(filename, "r") as f:
+			data: Dict[str, str]=json.load(f)
+			self.update_str(data.items())
+
+	@with_lock
+	def _save(self, filename: str)->None:
+		json.dump(
+				dict(self._cursor.execute("select outline, translation from dict")),
+				open(filename, "w"),
+				indent=0,
+				ensure_ascii=False,
+				)
